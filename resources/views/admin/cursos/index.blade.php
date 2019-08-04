332b04274e63982af257a0cd3ad9c89dfe11903e
Grazziano Fagundes <grazzianofagundes@gmail.com>
2019-08-04 14:28:05 -0300

    Layout com Materialize e lista de registros

---
+++ resources/views/admin/cursos/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layout.site')
+@section('titulo', 'Cursos')
+@section('conteudo')
+        
+<div class="container">
+    <h3 class="center">Lista de Cursos</h3>
+
+    <div class="row">
+        <table>
+            <thead>
+                <th>Id</th>
+                <th>Título</th>
+                <th>Descrição</th>
+                <th>Imagem</th>
+                <th>Publicado</th>
+                <th>Ação</th>
+            </thead>
+
+            <tbody>
+                @foreach ($registros as $registro)
+                    
+                <tr>
+                <td>{{$registro->id}}</td>
+                <td>{{$registro->titulo}}</td>
+                <td>{{$registro->descricao}}</td>
+                <td><img width="120" src="{{asset($registro->imagem)}}" alt="{{$registro->titulo}}"></td>
+                <td>{{$registro->publicado}}</td>
+                <td>
+                <a href="{{route('admin.cursos.editar', $registro->id)}}" class="btn deep-orange">Editar</a>
+                <a href="{{route('admin.cursos.deletar', $registro->id)}}" class="btn red">Deletar</a>
+                </td>
+                </tr>
+
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <div class="row">
+    <a href="{{route('admin.adicionar')}}" class="btn blue">Adicionar</a>
+    </div>
+
+</div>
+
+@endsection
