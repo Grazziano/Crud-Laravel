d7fbb40c47b45285ffda73ad0bcca7c01e93a4f6
Grazziano Fagundes <grazzianofagundes@gmail.com>
2019-08-05 00:12:36 -0300

    Deletar registros

--- resources/views/admin/cursos/index.blade.php
+++ resources/views/admin/cursos/index.blade.php
@@ -23,7 +23,7 @@
                 <td>{{$registro->id}}</td>
                 <td>{{$registro->titulo}}</td>
                 <td>{{$registro->descricao}}</td>
-                <td><img width="120" src="{{asset($registro->imagem)}}" alt="{{$registro->titulo}}"></td>
+                <td><img height="60" src="{{asset($registro->imagem)}}" alt="{{$registro->titulo}}"></td>
                 <td>{{$registro->publicado}}</td>
                 <td>
                 <a href="{{route('admin.editar', $registro->id)}}" class="btn deep-orange">Editar</a>
